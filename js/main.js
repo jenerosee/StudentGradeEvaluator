a6003476164389007b701c92fe17445f6f4922f1
jenerosee <60804516+jenerosee@users.noreply.github.com>
2020-03-31 16:56:40 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ function getRemark() {
     }
     else
      {
-         document.getElementById("rank-h").textContent="None";
+         document.getElementById("rank-h").textContent="none";
      }
 }
 
